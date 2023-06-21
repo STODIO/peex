f3d62ea63795939d76f22a46969874064c361b8b
Chang-Man <changagim@snu.ac.kr>
2023-06-21 22:11:51 +0900

    sass setup

---
+++ src/pages/_app.tsx
@@ -0,0 +1,5 @@
+import "@/styles/style.scss";
+import type { AppProps } from "next/app";
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Example</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Example</h1>
+    </>
+  );
+}
