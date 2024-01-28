3cf221d47aef4cabc20022b554feb5fb2f373955
김창아 <changagim@snu.ac.kr>
2024-01-28 22:04:49 +0900

    session provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,19 @@
+import { Metadata } from "next";
+import { SessionProvider } from "next-auth/react";
+
+export const metadata: Metadata = {
+  title: "title",
+  description: "database",
+  metadataBase: new URL(process.env.BASE_URL as string),
+  openGraph: {
+    title: "title",
+    description: "descsription",
+  },
+  alternates: {
+    canonical: "https://url",
+  },
+};
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +21,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="kr">
-      <body>{children}</body>
+      <body>
+        <SessionProvider>{children}</SessionProvider>
+      </body>
     </html>
   );
 }
