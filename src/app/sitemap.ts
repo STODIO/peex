978056b2b15a3e70fe5474d44a889515f5bfd5d9
김창아 <changagim@snu.ac.kr>
2024-01-29 01:06:18 +0900

    임시

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,11 +2,11 @@ import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
-    {
-      url: "https://domain",
-      lastModified: new Date(),
-      changeFrequency: "yearly",
-      priority: 1,
-    },
+    // {
+    //   url: "https://domain",
+    //   lastModified: new Date(),
+    //   changeFrequency: "yearly",
+    //   priority: 1,
+    // },
   ];
 }
