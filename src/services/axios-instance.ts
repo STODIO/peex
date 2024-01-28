5861b8e9395d51ed7dd23a48c6e402cc88a131c6
김창아 <changagim@snu.ac.kr>
2024-01-28 22:11:10 +0900

    add files

---
+++ src/services/axios-instance.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export const axiosAuth = axios.create();
+export const axiosNonAuth = axios.create();
+
+axiosAuth.defaults.baseURL = process.env.BACKEND_ENDPOINT;
+axiosNonAuth.defaults.baseURL = process.env.BACKEND_ENDPOINT;
+
+axiosAuth.defaults.headers.common["Content-Type"] = "application/json";
