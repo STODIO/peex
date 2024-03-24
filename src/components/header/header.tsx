0d675dacebcd44bafc66728eff4b2b44f9a323de
김창아 <changagim@snu.ac.kr>
2024-03-24 15:09:42 +0900

    menu

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,5 +1,56 @@
+'use client';
+import Image from 'next/image';
 import styles from './header.module.scss';
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
 
 export default function Header() {
-  return <header className={styles.header_wrapper}></header>;
+  const pathname = usePathname();
+
+  return (
+    <header className={styles.header_wrapper}>
+      <Link href={'/'}>
+        <div className={styles.logo_wrapper}>
+          <Image
+            src={'/images/logo.webp'}
+            alt={'stodio-logo'}
+            priority={true}
+            fill
+            style={{ objectFit: 'cover' }}
+          />
+        </div>
+      </Link>
+
+      <div className={styles.tab_wrapper}>
+        <Link href={'/'}>
+          <span
+            className={`${styles.tab} ${
+              pathname === '/' ? styles.selected : ''
+            }`}
+          >
+            ABOUT
+          </span>
+        </Link>
+        <Link href={'/business'}>
+          <span
+            className={`${styles.tab} ${
+              pathname.includes('business') ? styles.selected : ''
+            }`}
+          >
+            BUSINESS
+          </span>
+        </Link>
+        <Link href={'contact'}>
+          <span
+            className={`${styles.tab} ${
+              pathname.includes('contact') ? styles.selected : ''
+            }`}
+          >
+            CONTACT
+          </span>
+        </Link>
+      </div>
+      <div className={'desktop'} />
+    </header>
+  );
 }
