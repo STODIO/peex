742183aae2d67bab2467a4f6842f45f85a12da5f
김창아 <changagim@snu.ac.kr>
2024-03-24 16:11:25 +0900

    header 수정

--- src/containers/home/index.tsx
+++ src/containers/home/index.tsx
@@ -1,7 +1,12 @@
 import styles from './home.module.scss';
 
 const Home = () => {
-  return <div className={styles.wrapper}></div>;
+  return (
+    <>
+      <div className={styles.wrapper}></div>
+      <div className={styles.wrapper}></div>
+    </>
+  );
 };
 
 export default Home;
