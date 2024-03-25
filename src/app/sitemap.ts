47e137b1f5f536bd128b4d15834c896a72a101f5
김창아 <changagim@snu.ac.kr>
2024-03-25 12:07:53 +0900

    sitemap 완료

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,11 +2,23 @@ import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
-    // {
-    //   url: "https://domain",
-    //   lastModified: new Date(),
-    //   changeFrequency: "yearly",
-    //   priority: 1,
-    // },
+    {
+      url: 'https://peex.kr',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: 'https://peex.kr/business',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://peex.kr/contact',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
   ];
 }
